1735b6508d58801d975e2756b48d5e91138e786b
AbduraimovErlan <96403251+AbduraimovErlan@users.noreply.github.com>
2023-06-14 19:42:12 -0400

    APIView

--- book_api/urls.py
+++ book_api/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from book_api.views import book_list, book_create, book
+from book_api.views import BookList, BookCreate, BookDetail
+# from book_api.views import book_list, book_create, book
 from . import views
 
 
 
 urlpatterns = [
-    path('', views.book_create),
-    path('list/', views.book_list),
-    path('<int:pk>/', book),
+    path('', BookCreate.as_view()),
+    path('list/', BookList.as_view()),
+    path('<int:pk>/', BookDetail.as_view()),
 
 ]
--- book_api/views.py
+++ book_api/views.py
@@ -4,49 +4,95 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 from book_api.serializers import BookSerializer
-
-@api_view(['GET'])
-def book_list(request):
-    books = Book.objects.all()  # Complex Data
-    serializer = BookSerializer(books, many=True)
-    return Response(serializer.data)
+from rest_framework.views import APIView
 
 
-@api_view(['POST'])
-def book_create(request):
-    serializer = BookSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
+
+class BookList(APIView):
+    def get(self, request):
+        books = Book.objects.all()  # Complex Data
+        serializer = BookSerializer(books, many=True)
         return Response(serializer.data)
-    else:
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class BookCreate(APIView):
+    def post(self, request):
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def book(request, pk):
-    try:
-        book = Book.objects.get(pk=pk)
-    except:
-        return Response({
-            'error': 'Book does not exist'
-        }, status=status.HTTP_404_NOT_FOUND)
+class BookDetail(APIView):
+    def get_book_by_pk(self, pk):
+        try:
+            return Book.objects.get(pk=pk)
+        except:
+            return Response({
+                'error': 'Book does not exist'
+            }, status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == "GET":
-
+    def get(self, request, pk):
+        book = self.get_book_by_pk(pk=pk)
         serializer = BookSerializer(book)
         return Response(serializer.data)
 
-    if request.method == "PUT":
+    def put(self, request, pk):
         book = Book.objects.get(pk=pk)
-
         serializer = BookSerializer(book, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    if request.method == "DELETE":
-        book = Book.objects.get(pk=pk)
-
+    def delete(self, request, pk):
+        book = self.get_book_by_pk(pk)
         book.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+# @api_view(['GET'])
+# def book_list(request):
+#     books = Book.objects.all()  # Complex Data
+#     serializer = BookSerializer(books, many=True)
+#     return Response(serializer.data)
+#
+#
+# @api_view(['POST'])
+# def book_create(request):
+#     serializer = BookSerializer(data=request.data)
+#     if serializer.is_valid():
+#         serializer.save()
+#         return Response(serializer.data)
+#     else:
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def book(request, pk):
+#     try:
+#         book = Book.objects.get(pk=pk)
+#     except:
+#         return Response({
+#             'error': 'Book does not exist'
+#         }, status=status.HTTP_404_NOT_FOUND)
+#
+#     if request.method == "GET":
+#
+#         serializer = BookSerializer(book)
+#         return Response(serializer.data)
+#
+#     if request.method == "PUT":
+#         book = Book.objects.get(pk=pk)
+#
+#         serializer = BookSerializer(book, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     if request.method == "DELETE":
+#         book = Book.objects.get(pk=pk)
+#
+#         book.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
